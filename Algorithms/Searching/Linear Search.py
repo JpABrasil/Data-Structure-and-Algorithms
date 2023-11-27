a55d5e5fe1dd0546b2b1bb6ad76c14f0b6bbd286
JpABrasil <joaopedroalexandrino6@hotmail.com>
2023-11-27 09:53:37 -0300

    Updating the Linear search implementation

--- Algorithms/Searching/Linear Search.py
+++ Algorithms/Searching/Linear Search.py
@@ -1,12 +1,17 @@
-def searc(arr, N,x):
+def search(arr,x):
     '''
     arr: The array or list where you want to seach
     N: (int) number of the elements the arr have
     x: Target value you want to find
     '''
+    N = len(arr)
     for i in range(0,N):        #For i(index) in the range from 0 to N
         if (arr[i] == x):       #If the value of the array in that i(index) is equal to the target
             return i            #Return the index
     return -1                   #If not return -1
 
 #Testing the implementation
+array = [20,30,50,70,80]
+target = 30
+index_of_the_target=search(array,target)
+print(index_of_the_target)
