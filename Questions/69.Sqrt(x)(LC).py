75e58960dbad2aa2dd2139f7077e9a6b27380255
João Pedro <joaopedroalexandrino6@hotmail.com>
2023-11-30 17:06:12 -0300

    Adding new binary search Question

---
+++ Questions/69.Sqrt(x)(LC).py
@@ -0,0 +1,22 @@
+class Solution:
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        l=1
+        r=x
+        while l<=r:
+            mid = (l+r)//2
+            if mid*mid == x:
+                return mid
+            elif mid*mid < x:
+                l = mid +1
+            else:
+                r = mid-1
+        return r   
+            
+        
+
+
+
