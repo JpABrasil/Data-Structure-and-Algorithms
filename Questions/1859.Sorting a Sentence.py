eee9f134aafde85c9625a14d531db9b7f3b6f22e
João Pedro <joaopedroalexandrino6@hotmail.com>
2023-12-03 00:52:05 -0300

    Adding a 2 new sorting questions/ 1859 i don't finished yet

---
+++ Questions/1859.Sorting a Sentence.py
@@ -0,0 +1,8 @@
+
+
+def sortSentence(s):
+    words = s.split()
+    
+
+        
+sortSentence('is2 a3 This1 sentece4')
---
+++ Questions/2824. Count Pairs Whose Sum is Less than target.py
@@ -0,0 +1,7 @@
+def countPairs(nums,target):
+    count = 0
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if(i<j) and (nums[i] + nums[j] < target):
+                count = count+1 
+    return count
