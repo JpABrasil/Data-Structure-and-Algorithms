230a5eda9033804dc6f274a9495ad7b533f00928
João Pedro <joaopedroalexandrino6@hotmail.com>
2023-12-05 23:07:12 -0300

    Updating Math Question

--- Questions/Easy/1716. Calculate Money in LeetCode Bankl.py
+++ Questions/Easy/1716. Calculate Money in LeetCode Bankl.py
@@ -1,2 +1,11 @@
 def totalMoney(n):
-    money = []
+    total = 0
+    for i in range(n):
+        if i % 7 == 0:  
+            segunda= (i // 7) + 1  
+        total += segunda + (i % 7)  
+        print(total)
+
+totalMoney(10)
+
+    
